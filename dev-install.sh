f495637505b14de55e0728ab18d339365c44a27a
Albert Rovira <krandalf75@gmail.com>
2017-01-06 00:52:10 +0100

    Update dev-install.sh

--- dev-install.sh
+++ dev-install.sh
@@ -8,7 +8,11 @@ sudo apt-get update
 sudo apt-get install oracle-java8-installer
 sudo apt install oracle-java8-set-default
 
+# MAVEN
+sudo apt-get install maven
+
 # Netbeans 8.2
 sudo apt-get install wget
 sudo wget http://download.netbeans.org/netbeans/8.2/final/bundles/netbeans-8.2-javaee-linux.sh
+JAVA_HOME=/usr/lib/jvm/java-8-oracle
 sudo sh ./netbeans-8.2-javaee-linux.sh
