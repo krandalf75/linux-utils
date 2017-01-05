584bea9b77323271c2d943c748b1c96d92df4a61
Albert Rovira <krandalf75@gmail.com>
2017-01-05 22:18:47 +0100

    Update dev-install.sh

--- dev-install.sh
+++ dev-install.sh
@@ -1,3 +1,7 @@
 sudo apt-get update
 sudo apt-get install ubuntu-desktop
 sudo apt-get install default-jdk
+
+sudo apt-get install wget
+sudo wget http://download.netbeans.org/netbeans/8.2/final/bundles/netbeans-8.2-javaee-linux.sh
+sudo ./etbeans-8.2-javaee-linux.sh
