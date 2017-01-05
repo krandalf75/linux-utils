f14f0c2e40f6109dc13cf0559a2bc4cec013d3b8
Albert Rovira <krandalf75@gmail.com>
2017-01-05 23:10:52 +0100

    Update dev-install.sh

--- dev-install.sh
+++ dev-install.sh
@@ -1,7 +1,14 @@
 sudo apt-get update
 sudo apt-get install ubuntu-desktop
-sudo apt-get install default-jdk
+#sudo apt-get install default-jdk
+
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
 
 sudo apt-get install wget
 sudo wget http://download.netbeans.org/netbeans/8.2/final/bundles/netbeans-8.2-javaee-linux.sh
-sudo ./etbeans-8.2-javaee-linux.sh
+
+sudo sh ./netbeans-8.2-javaee-linux.sh
