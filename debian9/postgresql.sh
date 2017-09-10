5d033b3b433755c050d56c8e48f5401b433efd91
Albert Rovira <krandalf75@gmail.com>
2017-09-10 22:54:16 +0200

    Update postgresql.sh

--- debian9/postgresql.sh
+++ debian9/postgresql.sh
@@ -1,3 +1,5 @@
-apt-get install -y postgresql postgresql-client postgis pgadmin3
-sudo -u postgres bash
-adduser <xxxx>
+apt-get install -y postgresql postgresql-client postgis pgadmin3 osm2pgsql
+sudo -u postgres createdb -O postgres spain
+sudo -u postgres psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;" spain
+wget http://download.geofabrik.de/europe/spain-latest.osm.pbf
+osm2pgsql -s -U postgres -d spain spain-latest.osm-pbf
