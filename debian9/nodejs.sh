7df555538834ff75c05204f9d0a9de73e66c1c70
Albert Rovira <krandalf75@gmail.com>
2017-09-10 02:54:42 +0200

    Update nodejs.sh

--- debian9/nodejs.sh
+++ debian9/nodejs.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -Y curl software-properties-common build-essential
+sudo apt-get install -y curl software-properties-common build-essential
 curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
 sudo apt-get install -y nodejs
 
